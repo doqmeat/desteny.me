f8e08f904259dfaf4eb89de66c9ecf5b7b08a06e
Desteny <desteny282@gmail.com>
2025-01-02 17:08:21 -0400

    js function to pages for header

---
+++ desteny.me/header.js
@@ -0,0 +1,10 @@
+const text = `
+  <h1>❁ des' portfolio ❁</h1>
+  <nav>
+  <a href="/">about</a>
+  <a href="projects.html">projects</a>
+  <a href="comm.html">commisions</a>
+  <a href="contact.html">contact</a>
+  </nav>
+`;
+document.querySelector("header").innerHTML = text;
