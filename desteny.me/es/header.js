ff36a9100716c20220687940a400c8255af6d04a
doqmeat <desteny282@gmail.com>
2025-04-12 23:16:07 -0400

    change it up a bit

--- desteny.me/es/header.js
+++
@@ -1,9 +0,0 @@
-const text = `
-  <h1>❁ desteny.me ❁</h1>
-  <nav>
-  <a href="index.html">index</a>
-  <a href="projects.html">proyectos</a>
-  <a href="contact.html">contacto</a>
-  </nav>
-`;
-document.querySelector("header").innerHTML = text;
