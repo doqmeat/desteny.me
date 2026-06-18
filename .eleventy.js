766bb47633507323de5dbf621a91f64aa8dc309d
doqmeat <desteny282@gmail.com>
2026-06-18 01:54:28 -0400

    some updates

--- .eleventy.js
+++ .eleventy.js
@@ -23,22 +23,22 @@ module.exports = function (eleventyConfig) {
 		return DateTime.fromJSDate(dateObj).toFormat("dd.MMM.yy");
 	});
 
-	// RSS
-	eleventyConfig.addPlugin(pluginRss, {
-		type: "rss", // or "rss", "json"
-		outputPath: "/feed.xml",
-		collection: {
-			name: "posts", // iterate over `collections.posts`
-			limit: 10, // 0 means no limit
-		},
-		metadata: {
-			language: "en",
-			title: "desteny's RSS",
-			subtitle: "portfolio-ish site",
-			base: "https://desteny.me",
-			author: {
-				name: "des",
-			},
-		},
-	});
+	// // RSS
+	// eleventyConfig.addPlugin(pluginRss, {
+	// 	type: "rss", // or "rss", "json"
+	// 	outputPath: "/feed.xml",
+	// 	collection: {
+	// 		name: "posts", // iterate over `collections.posts`
+	// 		limit: 10, // 0 means no limit
+	// 	},
+	// 	metadata: {
+	// 		language: "en",
+	// 		title: "desteny's RSS",
+	// 		subtitle: "portfolio-ish site",
+	// 		base: "https://desteny.me",
+	// 		author: {
+	// 			name: "des",
+	// 		},
+	// 	},
+	// });
 };
